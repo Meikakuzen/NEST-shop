4e35400b0206f9515028073661bee1d5f467cda5
meikakuzen <meikakuzen@outlook.com>
2022-09-20 13:06:02 +0200

    nest

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,16 @@
+import { IsNumber, IsOptional, IsPositive } from "class-validator";
+import {Type} from 'class-transformer'
+
+export class PaginationDto{
+
+    @IsOptional()
+    @IsPositive()
+    @Type(()=> Number)
+    limit?: number;
+
+    @IsOptional()
+    @IsPositive()
+    @Type(()=> Number)
+    offset?: number
+
+}
